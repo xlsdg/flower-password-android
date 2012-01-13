ff07c2439e41a6622cb7dd51c862dcef9be89746
JohnnyJianHY@gmail.com <JohnnyJianHY@gmail.com@31db8dd1-3ab7-e112-c889-3edafd56e487>
2012-01-13 15:12:48 +0000

    Initial layout design

--- src/com/fpassword/android/MainActivity.java
+++ src/com/fpassword/android/MainActivity.java
@@ -1,7 +1,9 @@
 package com.fpassword.android;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends FragmentActivity {
 
@@ -9,6 +11,13 @@ public class MainActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
     }
 
 }
