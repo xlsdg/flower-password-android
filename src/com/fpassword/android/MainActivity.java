dd4bf507e612e7c023ef1ef3565dfff45dd9da31
JohnnyJianHY@gmail.com <JohnnyJianHY@gmail.com@31db8dd1-3ab7-e112-c889-3edafd56e487>
2012-01-13 11:26:08 +0000

    Add the action bar

---
+++ src/com/fpassword/android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fpassword.android;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+}
