49f025c4441355711a671144a6edf501ca4fdabe
JohnnyJianHY@gmail.com <JohnnyJianHY@gmail.com@31db8dd1-3ab7-e112-c889-3edafd56e487>
2012-01-14 11:21:24 +0000

    Implement MainActivity

---
+++ src/com/fpassword/android/FlowerPassword.java
@@ -0,0 +1,77 @@
+package com.fpassword.android;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import android.util.Log;
+
+public class FlowerPassword {
+
+    private static final String TAG = FlowerPassword.class.getName();
+
+    private static final String UTF_8 = "UTF-8";
+
+    private static final String HMAC_MD5 = "HmacMD5";
+
+    public static String encrypt(String passwordText, String keyText) {
+        if (passwordText.length() > 0 && keyText.length() > 0) {
+            try {
+                String md5one = hmacMd5(passwordText, keyText);
+                String md5two = hmacMd5(md5one, "snow");
+                String md5three = hmacMd5(md5one, "kise");
+                String rule = md5three;
+                StringBuilder source = new StringBuilder(md5two);
+                final String str = "sunlovesnow1990090127xykab";
+                for (int i = 0; i <= 31; ++i) {
+                    if (!Character.isDigit(source.charAt(i))) {
+                        if (str.indexOf(rule.charAt(i)) > -1) {
+                            source.setCharAt(i, Character.toUpperCase(source.charAt(i)));
+                        }
+                    }
+                }
+                String code32 = source.toString();
+                char code1 = code32.charAt(0);
+                String code16 = null;
+                if (!Character.isDigit(code1)) {
+                    code16 = code32.substring(0, 16);
+                } else {
+                    code16 = "K" + code32.substring(1, 16);
+                }
+                return code16;
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+        }
+        return "";
+    }
+
+    private static String hmacMd5(String dataText, String keyText) throws UnsupportedEncodingException,
+            NoSuchAlgorithmException, InvalidKeyException {
+        byte[] dataBytes = dataText.getBytes(UTF_8);
+        byte[] keyBytes = keyText.getBytes(UTF_8);
+
+        Mac mac = Mac.getInstance(HMAC_MD5);
+        Key key = new SecretKeySpec(keyBytes, HMAC_MD5);
+        mac.init(key);
+
+        byte[] resultBytes = mac.doFinal(dataBytes);
+        String resultText = encodeHexString(resultBytes);
+        return resultText;
+    }
+
+    private static String encodeHexString(byte[] data) {
+        int l = data.length;
+        char[] out = new char[l << 1];
+        for (int i = 0, j = 0; i < l; i++) {
+            out[j++] = Integer.toHexString((0xF0 & data[i]) >>> 4).charAt(0);
+            out[j++] = Integer.toHexString(0x0F & data[i]).charAt(0);
+        }
+        return new String(out);
+    }
+
+}
--- src/com/fpassword/android/MainActivity.java
+++ src/com/fpassword/android/MainActivity.java
@@ -3,14 +3,77 @@ package com.fpassword.android;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.Menu;
+import android.support.v4.view.MenuItem;
+import android.text.ClipboardManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+@SuppressWarnings("deprecation")
 public class MainActivity extends FragmentActivity {
+
+    private EditText editPassword;
+
+    private EditText editKey;
+
+    private EditText editResult;
+
+    private final TextWatcher textWatcher = new TextWatcher() {
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String passwordText = editPassword.getText().toString();
+            String keyText = editKey.getText().toString();
+            String resultText = FlowerPassword.encrypt(passwordText, keyText);
+            editResult.setText(resultText);
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        editPassword = (EditText) findViewById(R.id.edit_password);
+        editKey = (EditText) findViewById(R.id.edit_key);
+        editResult = (EditText) findViewById(R.id.edit_result);
+        final Button buttonCopy = (Button) findViewById(R.id.button_copy);
+
+        editPassword.addTextChangedListener(textWatcher);
+        editKey.addTextChangedListener(textWatcher);
+        buttonCopy.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                copyResultToClipboard();
+            }
+
+        });
+    }
+
+    private void copyResultToClipboard() {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        clipboard.setText(editResult.getText());
+        Toast.makeText(this, R.string.toast_copy_success, Toast.LENGTH_SHORT).show();
+    }
+
+    private void resetPasswordAndKey() {
+        editPassword.setText("");
+        editKey.setText("");
     }
 
     @Override
@@ -18,6 +81,17 @@ public class MainActivity extends FragmentActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.menu_item_reset:
+            resetPasswordAndKey();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 
 }
